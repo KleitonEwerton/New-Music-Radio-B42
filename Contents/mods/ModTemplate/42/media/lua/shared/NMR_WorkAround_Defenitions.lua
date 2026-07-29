93b66e15e6ff35d53b1e6d485a3e3264984e6b6a
Kleiton Ewerton <kleitonewertonoliveira@gmail.com>
2026-07-29 13:05:11 -0300

    feat: add global music table definitions and helper function for unified track keys

--- Contents/mods/ModTemplate/42/media/lua/shared/NMR_WorkAround_Defenitions.lua
+++ Contents/mods/ModTemplate/42/media/lua/shared/NMR_WorkAround_Defenitions.lua
@@ -1,2 +1,15 @@
 if not TCMusic then TCMusic = {} end
 if (GlobalMusic == nil) then GlobalMusic = {} end
+if not NMMusic then NMMusic = {} end
+
+if not NMMusic.buildUnifiedMusicKeys then
+    NMMusic.buildUnifiedMusicKeys = function()
+        local keys = {}
+        if NMTrackCatalog and NMTrackCatalog.entries then
+            for k, _ in pairs(NMTrackCatalog.entries) do
+                table.insert(keys, k)
+            end
+        end
+        return keys
+    end
+end
